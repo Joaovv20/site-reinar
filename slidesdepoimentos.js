51120f19b046bdadcebf82821dfbee34a874c88f
Joaovv20 <joaovitor98540@gmail.com>
2024-09-08 22:42:57 -0300

    First commit

---
+++ slidesdepoimentos.js
@@ -0,0 +1,8 @@
+let slide2 = document.querySelector(".slider2")
+let next2 = document.querySelector("#nextSlider")
+
+next2.addEventListener('click', function () {
+    let slides2 = document.querySeletorAll(".slides2")
+    slide2.appendChild(slides2[0])
+})
+const elemento = document.getElementById('elemento');
